d1bd5629d2665e4e662c98ee974931b128710d7a
sejun98 <tmdfl980520@naver.com>
2023-07-17 01:46:26 +0900

    first commit

---
+++ pages/section01/01-01-example/index.js
@@ -0,0 +1,13 @@
+function 나만의페이지(){
+    //js 위쪽은 js
+    const aaa = 3  
+
+    // html
+    return (
+        <div>
+            <div>철수</div>
+            <button>클릭하세요!!</button>
+        </div>
+    )
+
+}
