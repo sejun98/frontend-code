57d6b3192eef7367a993603e1c3ac793cca6c387
sejun98 <tmdfl980520@naver.com>
2023-07-18 05:27:25 +0900

    twoday commit

--- styles/01-02-emotion.js
+++ styles/01-02-emotion.js
@@ -1,5 +1,11 @@
 import styled from "@emotion/styled";
 
+export const Wrapper = styled.div`
+    width: 1200px;
+    box-shadow: 1px;
+    background-color: #999;
+`
+
 export const MyEmail = styled.span`
     color:blue;
     font-size : 25px;
@@ -7,3 +13,4 @@ export const MyEmail = styled.span`
 export const MyEmailInput = styled.input`
     border-color: red;
 `
+
